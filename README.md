8d4d35f202fd2ede136a0183ace321892d8d7d6d
Thomas Waterfall <thomas.waterfall@gmail.com>
2020-06-05 19:32:34 +0100

    Works

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # telegram-bot
-Quick Servlerless telegram bot
+
+Servlerless telegram echo bot using
+
+curl --request POST --url https://api.telegram.org/bot{token}/setWebhook --header 'content-type: application/json' --data '{"url": "{end-point}"}'
--- handler.ts
+++ handler.ts
@@ -1,5 +1,7 @@
 "use strict";
 import { APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
+import fetch from "node-fetch";
+
 export const hello: APIGatewayProxyHandler = async (event, context) => {
   return new Promise<APIGatewayProxyResult>((resolve, reject) =>
     setTimeout(
@@ -14,4 +16,20 @@ export const hello: APIGatewayProxyHandler = async (event, context) => {
       500
     )
   );
+};
+
+export const echoBot: APIGatewayProxyHandler = async (event, context) => {
+  const BASE_URL = `http://api.telegram.org/bot${process.env["TELEGRAM_TOKEN"]}/sendMessage`;
+  const body = JSON.parse(event.body!);
+  await fetch(
+    `${BASE_URL}?text=${encodeURI(body.message.text)}&chat_id=${encodeURI(
+      body.message.chat.id
+    )}`
+  );
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      input: event,
+    }),
+  };
 };
